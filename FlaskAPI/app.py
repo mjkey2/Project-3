83cb355c1a2824b1c4ed38b9fee88bf17faeb6e1
Mjkey2 <Mjmarentes2005@gmail.com>
2024-01-16 20:57:19 -0800

    Update app.py

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -1,8 +1,19 @@
 #import flask
 from flask import Flask, render_template, url_for
+from flask_sqlalchemy import SQLAlchemy
+from datetime import datetime
 #setup application (name references this file)
 app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///project3.db'
+db = SQLAlchemy(app)
 
+class Todo(db.Model):
+	id = db.Column(db.Integer, primary_key=True)
+	content = db.Column(db.String(200), nullable=False)
+	date_creates = db.Column(db.DateTime, default=datetime.utcnow)
+
+    def __repr__(self):
+        return '<Task %r>' % self.id
 #Define function for the index route so we don't 404 (give url string for route)
 #define function for the route, index returns a string.
 @app.route('/')
