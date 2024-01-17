a2c12ed559a5c4c27bca5d693261a6980fc94e9d
Mjkey2 <Mjmarentes2005@gmail.com>
2024-01-16 21:39:32 -0800

    Final changes for tuesday

--- FlaskAPI/app.py
+++ FlaskAPI/app.py
@@ -1,5 +1,5 @@
 #import flask
-from flask import Flask, render_template, url_for
+from flask import Flask, render_template, url_for, request, redirect
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
 #setup application (name references this file)
@@ -14,11 +14,26 @@ class Todo(db.Model):
 
     def __repr__(self):
         return '<Task %r>' % self.id
+
 #Define function for the index route so we don't 404 (give url string for route)
 #define function for the route, index returns a string.
-@app.route('/')
+@app.route('/', methods=['POST', 'GET'])
 def index():
-	return render_template('index.html')
+	if request.method == 'POST':
+		task_content = request.form['content']
+		new_task = Todo(content=task_content)
+		
+        try:
+            db.session.add(new_task)
+			db.session.commit()
+			return redirect('/')
+        except:
+            return 'There was an issue adding your task'
+	
+	else:
+		#you could also do .first to grab the first in the date but we aint doign that
+		tasks = Todo.query.order_by(Todo.date_created).all()
+		return render_template('index.html', tasks=tasks)
 
 if __name__ == "__main__":
 	#debug=true so if there are errors we can see on the webpage
